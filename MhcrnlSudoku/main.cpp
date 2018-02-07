315af24c25d56df8e8f6525685298b6895977bf8
mhcrnl <mhcrnl@gmail.com>
2018-02-07 18:08:19 +0200

    07.02.2018

--- MhcrnlSudoku/main.cpp
+++ MhcrnlSudoku/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  project 1 redo
 //
-//  Created by Ameya Pednekar on 
+//  Created by Ameya Pednekar on
 //  Copyright (c) 2013 Ameya Pednekar. All rights reserved.
 //
 
@@ -10,17 +10,17 @@
 
 using namespace std;
 
-#include "sudoku.hpp"
+#include "sudoku.h"
 int main()
 {
-    
-    
+
+
     sudoku sudokuBoard;
-	
+
     sudokuBoard.make_board();
     sudokuBoard.display();
-		  
-    
+
+
     return 0;
 }
 
